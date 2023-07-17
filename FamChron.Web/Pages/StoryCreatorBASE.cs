96055cd2b59785f723ef576c6d05dbf2df9627d9
Lubimec_ <dowslf@gmail.com>
2023-07-17 20:44:31 +0500

    Working on StoryCreator page

--- FamChron.Web/Pages/StoryCreatorBASE.cs
+++ FamChron.Web/Pages/StoryCreatorBASE.cs
@@ -4,6 +4,9 @@ namespace FamChron.Web.Pages
 {
     public class StoryCreatorBASE : ComponentBase
     {
-
+        public string storyName { get; set; }
+        public DateTime storyDate { get; set; }
+        public string description { get; set; }
+        public string previewIMG { get; set; }
     }
 }
