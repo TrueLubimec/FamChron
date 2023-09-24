2b3a00b044ad57a246abd67b2c80039d147e1cc5
Lubimec_ <dowslf@gmail.com>
2023-09-24 23:11:38 +0500

    jwt done

--- FamChron.Api/Controllers/AuthController.cs
+++ FamChron.Api/Controllers/AuthController.cs
@@ -2,6 +2,10 @@
 using FamChron.Models.Dtos;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace FamChron.Api.Controllers
 {
@@ -10,6 +14,12 @@ namespace FamChron.Api.Controllers
     public class AuthController : ControllerBase
     {
         public static User user = new User();
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         [HttpPost("register")]
         public ActionResult<User> Register (UserDto userDtoRequest)
@@ -26,6 +36,7 @@ namespace FamChron.Api.Controllers
         [HttpPost("login")]
         public ActionResult<User> Login(UserDto userDtoRequest)
         {
+            // лучше сделать один тест или одинаковые сообщения, чтобы сложнее ломануть
             if (user.UserName != userDtoRequest.Name)
             {
                 return BadRequest("User not found.");
@@ -35,7 +46,32 @@ namespace FamChron.Api.Controllers
                 return BadRequest("Wrong password.");
             }
 
-            return Ok(user);
+            string token = CreateToken(user);
+
+            return Ok(token);
+        }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, user.UserName)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                                       _configuration.GetSection("Jwt:Token").Value!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                    claims: claims,
+                    expires: DateTime.Now.AddDays(1),
+                    signingCredentials: creds
+                );
+
+            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
         }
     }
 }
