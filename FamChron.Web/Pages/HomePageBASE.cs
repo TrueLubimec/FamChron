d9bfb3bb851fba22490da8906785490c00bdc2f6
Lubimec_ <dowslf@gmail.com>
2023-10-15 22:16:15 +0500

    теперь сделать нормально отображение с пользователем

--- FamChron.Web/Pages/HomePageBASE.cs
+++ FamChron.Web/Pages/HomePageBASE.cs
@@ -1,8 +1,22 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using FamChron.Web.Services.Contracts;
+using Microsoft.AspNetCore.Components;
 
 namespace FamChron.Web.Pages
 {
     public class HomePageBASE : ComponentBase
     {
+        [Parameter]
+        public int id { get; set; }
+        
+        [Inject]
+        public ILoginService loginService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                loginService.
+            }
+        }
     }
 }
