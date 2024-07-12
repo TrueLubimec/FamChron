b9b3a77507b319c9c147bec1998eb265caf460a3
Lubimec_ <dowslf@gmail.com>
2024-07-12 12:54:46 +0300

    ocelot 1

--- FamChron.Gateway/Program.cs
+++ FamChron.Gateway/Program.cs
@@ -1,4 +1,6 @@
 using FamChron.Gateway.Protos;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,11 @@ builder.Services.AddGrpcClient<AuthenticationGrpcService.AuthenticationGrpcServi
     o.Address = new Uri("https://localhost:5059");
 });
 
+builder.Configuration.SetBasePath(builder.Environment.ContentRootPath)
+                     .AddJsonFile("configOcelot.json", optional: false, reloadOnChange: true)
+                     .AddEnvironmentVariables();
+builder.Services.AddOcelot(builder.Configuration);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -20,14 +27,20 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseSwaggerForOcelotUI(option =>
+    {
+        option.PathToSwaggerGenerator = "/swagger/docs";
+    });
 }
 
 app.UseHttpsRedirection();
 
-//app.UseAuthorization();
+app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+await app.UseOcelot();
+
+await app.RunAsync();
 
 
